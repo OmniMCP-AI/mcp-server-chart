f14b742a648b5a0eee06d08890723a00e98e04a1
rowland <1148859424@qq.com>
2025-11-19 14:28:52 +0800

    feat: support chineess font

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -3,8 +3,8 @@ import { zodToJsonSchema as zodToJsonSchemaOriginal } from "zod-to-json-schema";
 
 // TODO: use zod v4 JSON to schema to replace zod-to-json-schema when v4 is stable
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-export const zodToJsonSchema = (schema: Record<string, z.ZodType<any>>) => {
-  return zodToJsonSchemaOriginal(z.object(schema), {
+export const zodToJsonSchema = (schema: Record<string, z.ZodType<any>>): any => {
+  return zodToJsonSchemaOriginal(z.object(schema) as any, {
     rejectedAdditionalProperties: undefined,
     $refStrategy: "none",
   });
